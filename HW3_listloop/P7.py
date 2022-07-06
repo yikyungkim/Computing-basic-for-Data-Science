458f88e651c7b276c4a91ba682ca43b3ba34bd56
yikyungkim <k2y1513@snu.ac.kr>
2022-07-06 16:39:03 +0900

    Create P7.py

---
+++ HW3_listloop/P7.py
@@ -0,0 +1,9 @@
+def P7(L1: list) -> list:
+    ans_list = []
+    ##### Write your Code Here #####
+    
+    for i in range(len(L1)):
+        ans_list.append(L1[i]+1)
+
+    ##### End of your code #####
+    return ans_list
